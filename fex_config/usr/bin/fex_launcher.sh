7ea94267127c43bb1766e3a524f0b65e1a8f85c4
Mitchell Augustin <mitchell.augustin@canonical.com>
2025-12-02 10:43:38 -0600

    Rework launcher script

--- fex_config/usr/bin/fex_launcher.sh
+++ fex_config/usr/bin/fex_launcher.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-FEX_SERVERSOCKETPATH=/tmp/FEXServer.Socket FEX_APP_CONFIG_LOCATION=/snap/steam/current/fex_config/ /snap/steam/current/usr/bin/FEXBash $@
+FEX_SERVERSOCKETPATH=/var/snap/steam/current/FEXServer.Socket FEX_APP_CONFIG_LOCATION=/snap/steam/current/fex_config/ /snap/steam/current/usr/bin/FEXBash /snap/steam/current/usr/lib/steam/bin_steam.sh -no-cef-sandbox
