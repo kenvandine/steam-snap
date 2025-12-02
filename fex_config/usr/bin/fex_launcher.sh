285eae4d80b2aa85603a6a0736299d830df5bb4a
Mitchell Augustin <mitchell.augustin@canonical.com>
2025-12-02 14:57:45 -0600

    First working version with snap run steam

--- fex_config/usr/bin/fex_launcher.sh
+++ fex_config/usr/bin/fex_launcher.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-FEX_SERVERSOCKETPATH=/var/snap/steam/current/FEXServer.Socket FEX_APP_CONFIG_LOCATION=/snap/steam/current/fex_config/ /snap/steam/current/usr/bin/FEXBash /snap/steam/current/usr/lib/steam/bin_steam.sh -no-cef-sandbox
+FEX_SERVERSOCKETPATH=/home/$USER/snap/steam/common/.fex-emu/FEXServer.Socket FEX_APP_CONFIG_LOCATION=/snap/steam/current/fex_config/ /snap/steam/current/usr/bin/FEXBash /snap/steam/current/usr/lib/steam/bin_steam.sh -no-cef-sandbox
