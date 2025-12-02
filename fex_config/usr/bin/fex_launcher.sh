db907930527cb0bee9a8703792ae5090d3d63931
Mitchell Augustin <mitchell.augustin@canonical.com>
2025-12-02 15:27:03 -0600

    Create required config dir if it doesn't exist

--- fex_config/usr/bin/fex_launcher.sh
+++ fex_config/usr/bin/fex_launcher.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-FEX_SERVERSOCKETPATH=/home/$USER/snap/steam/common/.fex-emu/FEXServer.Socket FEX_APP_CONFIG_LOCATION=/snap/steam/current/fex_config/ /snap/steam/current/usr/bin/FEXBash /snap/steam/current/usr/lib/steam/bin_steam.sh -no-cef-sandbox
+mkdir -p /home/$USER/snap/steam/common/.fex-emu/
+export FEX_SERVERSOCKETPATH=/home/$USER/snap/steam/common/.fex-emu/FEXServer.Socket
+export FEX_APP_CONFIG_LOCATION=/snap/steam/current/fex_config/
+/snap/steam/current/usr/bin/FEXBash /snap/steam/current/usr/lib/steam/bin_steam.sh -no-cef-sandbox
