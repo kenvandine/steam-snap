bde515c382c4f0b42efb0d016fa2b19ef7bfc98d
Mitchell Augustin <mitchell.augustin@canonical.com>
2025-12-08 15:35:10 -0600

    Fix missing fex config directory

--- fex_config/usr/bin/fex_launcher.sh
+++ fex_config/usr/bin/fex_launcher.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p /home/$USER/snap/steam/common/.fex-emu/
+mkdir -p /home/$USER/.fex-emu/
 #export FEX_SERVERSOCKETPATH=/home/mitchell/snap/steam/current/.fex-emu/FEXServer.Socket
 export FEX_SERVERSOCKETPATH=/home/$USER/.fex-emu/FEXServer.Socket
 export FEX_APP_CONFIG_LOCATION=/snap/steam/current/fex_config/
